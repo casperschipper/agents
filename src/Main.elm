37527fbf43b8248e3134ebf6af62415ad3229493
Caspers QuadMBP <casper.schipper@gmail.com>
2021-05-28 10:23:03 +0200

    first working version

--- src/Main.elm
+++ src/Main.elm
@@ -1,8 +1,10 @@
 module Main exposing (Agent(..), Model, Msg(..), State(..), agentTrigger, generateAgents, init, main, randomAgent, randomAgents, subscriptions, update, view, viewAgent, viewTime)
 
+import Array
 import Browser
 import Browser.Events as Events
 import Html exposing (Html, pre, text)
+import Html.Attributes as Attr
 import Http
 import Platform.Cmd as Cmd
 import Random
@@ -34,7 +36,7 @@ main =
 
 
 type alias Model =
-    { agents : List Agent
+    { agents : Array.Array Agent
     , t : Int
     }
 
@@ -44,8 +46,15 @@ type State
     | Passive
 
 
+type alias AgentState =
+    { state : State
+    , threshold : Int
+    , connections : List Int
+    }
+
+
 type Agent
-    = Agent State Int
+    = Agent AgentState
 
 
 type Msg
@@ -54,33 +63,106 @@ type Msg
     | Tick Posix
 
 
-randomAgent : Random.Generator Agent
-randomAgent =
-    Random.int 0 100
-        |> Random.map (\i -> Agent Passive i)
+randomAgent : Int -> Int -> Random.Generator Agent
+randomAgent numConnections maxConnection =
+    let
+        connections =
+            Random.list 10 (Random.int 0 maxConnection)
+
+        thresh =
+            Random.int 0 20
+
+        rstate =
+            Random.weighted ( 90, Active ) [ ( 90, Passive ) ]
+    in
+    Random.map3 (\i cs state -> Agent (AgentState state i cs)) thresh connections rstate
+
+
+table : List (Html Msg) -> Html Msg
+table items =
+    let
+        n =
+            List.length items
+
+        side =
+            sqrt (toFloat n) |> floor
+
+        indexed =
+            List.map2 (\x y -> (x,y)) (List.range 0 n) items
+
+        rowNumbers =
+            List.range 0 side
+
+        rows =
+            List.map
+                (\nRow ->
+                    let
+                        i0 =
+                            nRow * side
+
+                        slice =
+                            List.filter (\( i, _ ) -> i > i0 && i < (i0 + side)) indexed |> List.map (\(_,x) -> x)
+
+                    in
+                    Html.tr [] (List.map (\item -> Html.td [] [item]) slice)
+                )
+                rowNumbers
+    in
+    Html.table [] rows
 
 
 randomAgents =
-    Random.list 100 randomAgent
+    let
+        num =
+            100
+
+        numConnections =
+            num // 10
+    in
+    Random.list num (randomAgent numConnections num)
 
 
 agentTrigger : Int -> Agent -> Agent
-agentTrigger value (Agent state thresh) =
-    case state of
+agentTrigger value (Agent st) =
+    case st.state of
         Active ->
-            Agent state thresh
+            Agent st
 
         Passive ->
-            if value > thresh then
-                Agent Active thresh
+            if value > st.threshold then
+                Agent { st | state = Active }
 
             else
-                Agent Passive thresh
+                Agent { st | state = Passive }
+
+
+evaluate : Agent -> Model -> Agent
+evaluate (Agent agent) model =
+    let
+        connectedAgents =
+            List.foldl
+                (\idx acc ->
+                    Array.get idx model.agents
+                        |> Maybe.map
+                            (\(Agent a) ->
+                                case a.state of
+                                    Active ->
+                                        1 + acc
+
+                                    Passive ->
+                                        acc
+                            )
+                        |> Maybe.withDefault acc
+                )
+                0
+                agent.connections
+    in
+    agentTrigger connectedAgents (Agent agent)
 
 
 init : () -> ( Model, Cmd Msg )
 init _ =
-    ( { t = 0, agents = [] }
+    ( { t = 0, agents = Array.empty }
     , generateAgents
     )
 
@@ -101,10 +183,22 @@ update msg model =
             ( model, Cmd.none )
 
         GotAgents agents ->
-            ( { model | agents = agents }, Cmd.none )
+            ( { model | agents = Array.fromList agents }, Cmd.none )
 
         Tick time ->
-            ( { model | t = model.t + 1 }, Cmd.none )
+            if remainderBy 60 model.t == 0 then
+                let
+                    newAgents =
+                        Array.map (\agent -> evaluate agent model) model.agents
+                in
+                    ( { model
+                          | t = model.t + 1
+                          , agents = newAgents
+                      }
+                    , Cmd.none
+                    )
+            else
+                ({ model | t = model.t + 1}, Cmd.none)
 
 
 
@@ -120,21 +214,28 @@ subscriptions model =
 -- VIEW
 
 
-viewAgent : Agent -> String
-viewAgent (Agent state thresh) =
+viewAgent : Agent -> Html Msg
+viewAgent (Agent state) =
     let
         t =
-            String.fromInt thresh
+            String.fromInt state.threshold |>
+                                             (\str ->
+                                                  if String.length str == 1 then
+                                                      "0" ++ str
+                                                  else str)
 
-        s =
-            case state of
+        (s,color) =
+            case state.state of
                 Active ->
-                    "x"
+                    ("x","green")
 
                 Passive ->
-                    "."
+                    (".","red")
+
+        string = [ "(", s, ":", t, ")" ] |> String.join "" 
     in
-    [ "(", s, ":", t, ")" ] |> String.join ""
+    Html.div [ Attr.style "color" color ] [ text string ] 
+        
 
 
 viewTime : Int -> Html Msg
@@ -144,12 +245,16 @@ viewTime x =
 
 view : Model -> Html Msg
 view model =
-    case model.agents of
+    let
+        lst =
+            Array.map viewAgent model.agents |> Array.toList
+    in
+    case lst of
         [] ->
-            text "nothing to see"
+            text "nothing to see here"
 
-        lst ->
+        ls ->
             Html.div []
                 [ viewTime model.t
-                , String.join " " (List.map viewAgent lst) |> text
+                , lst |> table
                 ]
