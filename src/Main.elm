e8a400c4ab83fd0ee65118cd81e3836cba4ac67d
Caspers QuadMBP <casper.schipper@gmail.com>
2021-05-30 17:54:16 +0200

    less interesting

--- src/Main.elm
+++ src/Main.elm
@@ -23,12 +23,21 @@ import Time exposing (Posix)
 -- MAIN
 
 
-totalNumber = 900
-nNeighbours = 8
-startCells = 1
-                          
-              
-                 
+totalNumber =
+    900
+
+
+nNeighbours =
+    8
+
+
+startCells =
+    1
+
+
+nRand =
+    2
+
 
 main =
     Browser.element
@@ -69,23 +78,30 @@ type Agent
 type Msg
     = NoOp
     | GotAgents (List Agent)
+    | AddConnection Int Int
     | Tick Posix
     | Reset
+    | Clicked Int
 
 
 randomAgent : Int -> Int -> Random.Generator Agent
 randomAgent numConnections maxConnection =
     let
         connections =
-            Random.list 10 (Random.int 0 maxConnection)
+            Random.list nRand (Random.int 0 maxConnection)
 
         thresh =
-            Random.int 1 14
+            Random.int 1 10
 
         rstate =
-            Random.weighted ( 10, Active ) [ ( 100, Passive ) ]
+            Random.weighted ( 90, Active ) [ ( 100, Passive ) ]
     in
     Random.map3 (\i cs state -> Agent (AgentState state i cs 0)) thresh connections rstate
+
+
+randomConnection : Random.Generator ( Int, Int )
+randomConnection =
+    Random.map2 (\x y -> ( x, y )) (Random.int 0 totalNumber) (Random.int 0 totalNumber)
 
 
 table : List (Html Msg) -> Html Msg
@@ -123,7 +139,7 @@ table items =
 randomAgents =
     let
         num =
-           totalNumber
+            totalNumber
 
         numConnections =
             2
@@ -131,29 +147,51 @@ randomAgents =
     Random.list num (randomAgent numConnections num)
 
 
+withState s (Agent st) =
+    Agent { st | state = s }
+
+
+flip s (Agent st) =
+    Agent
+        { st
+            | state =
+                case st.state of
+                    Active ->
+                        Passive
+
+                    Passive ->
+                        Active
+        }
+
+
+getState (Agent st) =
+    st.state
+
+
+getThreshold (Agent st) =
+    st.threshold
+
+
+withBirth birth (Agent st) =
+    Agent { st | birth = birth }
+
+
 agentTrigger : Int -> Int -> Agent -> Agent
-agentTrigger now value (Agent st) =
-    case st.state of
+agentTrigger now value agent =
+    case getState agent of
         Active ->
-            let age =
-                    (now - st.birth) > (value * 4)
-
-                overStimulated =
-                    value > (st.threshold + 1)
-            in
-            case (age,overStimulated) of
-                (True,True) ->
-                    Agent { st | state = Passive }
-
-                (_,_) ->
-                    Agent st
-
-        Passive ->
-            if value > st.threshold then
-                Agent { st | state = Active, birth = now}
+            if value > 10 then
+                agent |> withState Passive
 
             else
-                Agent { st | state = Passive }
+                agent
+
+        Passive ->
+            if value > getThreshold agent then
+                agent |> withState Active |> withBirth now
+
+            else
+                agent |> withState Passive
 
 
 evaluate : Agent -> Model -> Agent
@@ -217,6 +255,57 @@ neighbours n max index =
     List.range 0 n |> List.map ((\x -> x - (n // 2)) >> wrap 0 max)
 
 
+type Event
+    = Evaluate
+    | GrowConnection
+    | NoEvent
+
+
+fromTime : Int -> Event
+fromTime t =
+    if modBy 2 t == 0 then
+        GrowConnection
+
+    else
+        Evaluate
+
+
+addConnection : ( Int, Int ) -> Msg
+addConnection ( x, y ) =
+    AddConnection x y
+
+
+updateWithEvent : Event -> Model -> ( Model, Cmd Msg )
+updateWithEvent e model =
+    let
+        laterModel m =
+            { m | t = m.t + 1 }
+    in
+    case e of
+        Evaluate ->
+            let
+                _ =
+                    Debug.log "eval" m
+
+                newAgents =
+                    Array.map (\agent -> evaluate agent model) model.agents
+
+                m =
+                    laterModel model
+            in
+            ( { m
+                | agents = newAgents
+              }
+            , Cmd.none
+            )
+
+        GrowConnection ->
+            ( laterModel model, Random.generate addConnection randomConnection )
+
+        NoEvent ->
+            ( laterModel model, Cmd.none )
+
+
 update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
     case msg of
@@ -240,20 +329,48 @@ update msg model =
             ( { model | agents = Array.fromList connected }, Cmd.none )
 
         Tick time ->
-            if remainderBy 4 model.t == 0 then
-                let
-                    newAgents =
-                        Array.map (\agent -> evaluate agent model) model.agents
-                in
-                ( { model
-                    | t = model.t + 1
-                    , agents = newAgents
-                  }
-                , Cmd.none
-                )
+            updateWithEvent (fromTime model.t) model
 
-            else
-                ( { model | t = model.t + 1 }, Cmd.none )
+        AddConnection from to ->
+            let
+                _ =
+                    Debug.log "fish" ( from, to )
+
+                mAgent =
+                    Array.get from model.agents
+            in
+            case mAgent of
+                Just (Agent agent) ->
+                    let
+                        updated =
+                            Agent { agent | connections = agent.connections ++ [ to ] }
+
+                        newAgents =
+                            Array.set from updated model.agents
+                    in
+                    ( { model | agents = newAgents }, Cmd.none )
+
+                Nothing ->
+                    ( model, Cmd.none )
+
+        Clicked index ->
+            let
+                mAgent =
+                    Array.get index model.agents
+            in
+            case mAgent of
+                Nothing ->
+                    ( model, Cmd.none )
+
+                Just agent ->
+                    let
+                        active =
+                            agent |> withState Active
+
+                        newAgents =
+                            Array.set index active model.agents
+                    in
+                    ( { model | agents = newAgents }, Cmd.none )
 
 
 
@@ -269,12 +386,11 @@ subscriptions model =
 -- VIEW
 
 
-viewAgent : Agent -> Html Msg
-viewAgent (Agent state) =
+viewAgent : Int -> Agent -> Html Msg
+viewAgent index (Agent state) =
     let
         t =
             String.fromInt state.threshold
-     
 
         ( s, color ) =
             case state.state of
@@ -287,7 +403,12 @@ viewAgent (Agent state) =
         string =
             [ "(", s, ":", t, ")" ] |> String.join ""
     in
-    Html.div [ Attr.style "background-color" color, Attr.style "width" "20px" ] [ text string ]
+    Html.div
+        [ Attr.style "background-color" color
+        , Attr.style "width" "20px"
+        , E.onClick <| Clicked index
+        ]
+        [ text string ]
 
 
 viewTime : Int -> Html Msg
@@ -299,14 +420,14 @@ view : Model -> Html Msg
 view model =
     let
         lst =
-            Array.map viewAgent model.agents |> Array.toList
+            Array.indexedMap viewAgent model.agents |> Array.toList
     in
     case lst of
         [] ->
             text "nothing to see here"
 
         ls ->
-            Html.div [ Attr.style "font-size" "8px"]
+            Html.div [ Attr.style "font-size" "8px" ]
                 [ Html.button [ E.onClick Reset ]
                     [ text "reset" ]
                 , viewTime
